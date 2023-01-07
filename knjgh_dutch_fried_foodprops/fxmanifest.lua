558ab5e6541903f5c393055d5e66f7907fe5ee7a
knjgh <LC151371@gmail.com>
2023-01-07 17:30:28 +0100

    Add files via upload

---
+++ knjgh_dutch_fried_foodprops/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+games { 'gta5' }
+author 'KNJGH'
+description 'Dutch Fried Food'
+version '1.0.0'
+
+
+-- Loading YTYP (important for the script)
+data_file 'DLC_ITYP_REQUEST' 'stream/knjgh_dutch_snacks.ytyp'
